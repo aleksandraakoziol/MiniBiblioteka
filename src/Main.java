483056bb6732e960af5cb8980e6e54bd9a843a5a
aleksandraakoziol <s35332@pjwstk.edu.pl>
2026-03-24 23:00:05 +0100

    Dodanie klasy Czytelnik i metod pomocniczych

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        // Zostawiamy książkę z poprzedniego zadania
         Ksiazka k1 = new Ksiazka("Wiedźmin", "Andrzej Sapkowski", 350, true);
-        Ksiazka k2 = new Ksiazka("Hobbit", "J.R.R. Tolkien", 300, true);
-        Ksiazka k3 = new Ksiazka("1984", "George Orwell", 280, true);
 
-        k1.wypiszInfo();
-        k1.wypozycz();
-        System.out.println("Po wypożyczeniu:");
-        k1.wypiszInfo();
+        // Tworzymy czytelnika z Zadania 2
+        Czytelnik c1 = new Czytelnik("Jan", "Kowalski", 101);
+
+        // Testujemy czytelnika
+        c1.wypiszDane();
+        System.out.println("Symulujemy wypożyczenie...");
+        c1.zwiekszLiczbeWypozyczen();
+        c1.wypiszDane();
     }
 }
